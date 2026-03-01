bac20f25160715c1f4106307c4b11207fdcae50d
AmanCrafts <theamanmalikarts@gmail.com>
2026-03-01 06:49:56 +0530

    fixed prisma config

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,9 +1,10 @@
 import "dotenv/config";
-const { defineConfig } = require("prisma/config");
 
-module.exports = defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    url: process.env.DIRECT_URL!,
+export default {
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL!,
+      directUrl: process.env.DIRECT_URL!,
+    },
   },
-});
+};
